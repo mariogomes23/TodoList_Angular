769df0f0fc395a63ba41ea7b945cc1bf72cdce16
mario gomes <markkennedy232323@gmail.com>
2023-07-25 23:51:03 +0100

    inicio

---
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -0,0 +1,43 @@
+import { Component,OnInit,DoCheck } from '@angular/core';
+import { TaskList } from '../../model/task-list';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit,DoCheck {
+
+  public taskList:Array<TaskList> = [
+    {task:"Minnha nova",checked:true},
+    {task:"Minnha segunta tarefa",checked:false}
+  ]
+
+
+  ngOnInit(): void {
+
+  }
+  ngDoCheck(): void {
+
+    this.taskList.sort( (first,last)=>Number(first.checked)-Number(last.checked))
+  }
+
+  public deleteItemTaskList(event:number)
+  {
+     this.taskList.splice(event,1)
+  }
+
+  public deleteAllTaskList()
+  {
+    this.taskList=[]
+  }
+
+
+
+  public setEmitTaskList(event:string)
+  {
+
+    this.taskList.push({task:event,checked:false})
+  }
+
+}
